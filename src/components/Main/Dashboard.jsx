01e362543e5bd25222a16e3c92fcfc9d21bca995
Minel03 <saint.mynel@gmail.com>
2025-02-03 14:54:22 +0800

    centered dashboard and models

--- src/components/Main/Dashboard.jsx
+++ src/components/Main/Dashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className="p-6 bg-gray-100 min-h-screen flex flex-col items-center">
       <h1>Dashboard</h1>
       <p>Welcome to the Dashboard!</p>
     </div>
